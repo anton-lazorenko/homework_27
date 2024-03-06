ba22e689f35b6a7dc8f93f9d588e750f8c7a191c
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-03-06 17:08:54 +0200

    add method 2 for task 1

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+// Task 1
+
+// Method 1
+
 const books = [
     { title: 'Гаррі Поттер', author: 'Дж.К. Ролінг' },
     { title: '1984', author: 'Джордж Орвелл' },
     { title: 'Хоббіт', author: 'Дж.Р.Р. Толкієн' }
-   ];
-   
-   const ourString = books.filter(item => item.author === 'Джордж Орвелл');
-   console.log(ourString);
+];
+
+const ourString = books.filter(item => item.author === 'Джордж Орвелл');
+console.log(ourString);
+
+// Method 2  
+
+const ourbooks = [
+    { title: 'Гаррі Поттер', author: 'Дж.К. Ролінг' },
+    { title: '1984', author: 'Джордж Орвелл' },
+    { title: 'Хоббіт', author: 'Дж.Р.Р. Толкієн' }
+];
+
+ourbooks.some((find) => {
+    console.log(find)
+    return find.author === 'Джордж Орвелл'
+})
+
+// Task 2
