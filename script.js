65d7a074cd46f9aaac92c7e8cbfb27f1c4190659
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-03-06 18:43:20 +0200

    finished task 2

--- script.js
+++ script.js
@@ -25,4 +25,12 @@ ourbooks.some((find, index) => {
     return find.author === 'Джордж Орвелл'
 })
 
-// Task 2
+// Task 2   
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+const pairedNumbers = numbers.filter((num) => {
+    return num % 2 === 0
+});
+console.log(pairedNumbers);
+ 
+
