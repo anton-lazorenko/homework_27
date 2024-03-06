1c4adde466cea32e50a0935766f0a13075ff4256
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-03-06 19:23:20 +0200

    finished task 5

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ const ourbooks = [
 ];
 
 ourbooks.some((find, index) => {
-    console.log(find)
-    console.log(index)
-    return find.author === 'Джордж Орвелл'
+    console.log(find);
+    console.log(index);
+    return find.author === 'Джордж Орвелл';
 })
 
 // Task 2   
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
 const pairedNumbers = numbers.filter((num) => {
-    return num % 2 === 0
+    return num % 2 === 0;
 });
 console.log(pairedNumbers);
  
@@ -42,3 +42,17 @@ let newNumbers = prices.map((x) => x + (x * percent) / 100 );
 console.log(newNumbers);
  
 // Task 4  
+ 
+const arrNumbers = [45, 80, 32, 100, 105]; 
+arrNumbers.some((moreThan) =>{
+    console.log(moreThan);
+    return moreThan > 100;
+});
+
+// Task 5  
+
+const nums = [1, 2, 3, 4, 5, -6, 7];
+const exam = nums.every(item => item > 0);
+console.log(exam);
+
+// Task 6  
