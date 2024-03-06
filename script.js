de23d6a2a41373bb7506e5cee45d353cf0751818
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-03-06 21:41:56 +0200

    finished task 6

--- script.js
+++ script.js
@@ -27,24 +27,24 @@ ourbooks.some((find, index) => {
 
 // Task 2   
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pairedNumbers = numbers.filter((num) => {
     return num % 2 === 0;
 });
 console.log(pairedNumbers);
- 
+
 // Task 3  
 
 const prices = [100, 200, 300, 400, 500];
 let percent = 10
-let newNumbers = prices.map((x) => x + (x * percent) / 100 ); 
+let newNumbers = prices.map((x) => x + (x * percent) / 100);
 
 console.log(newNumbers);
- 
+
 // Task 4  
- 
-const arrNumbers = [45, 80, 32, 100, 105]; 
-arrNumbers.some((moreThan) =>{
+
+const arrNumbers = [45, 80, 32, 100, 105];
+arrNumbers.some((moreThan) => {
     console.log(moreThan);
     return moreThan > 100;
 });
@@ -56,3 +56,12 @@ const exam = nums.every(item => item > 0);
 console.log(exam);
 
 // Task 6  
+
+const sentences = ["Я люблю JavaScript", "Масиви це весело", "Програмування це круто"];
+
+const newSentences = sentences.map((sentence) => {
+    const words = sentence.split(" ");
+    return words[0];
+});
+
+console.log(newSentences);  
